504d4eac4bc0d6eeb1682cec3298ac117978c6df
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-05-17 22:44:00 +0530

    feat: implement runCodeForCollaborativeEditor endpoint for collaborative code execution

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -294,3 +294,116 @@ export const runCode = async (req, res) => {
     });
   }
 };
+
+export const runCodeForCollaborativeEditor = async (req, res) => {
+  try {
+    const { source_code, language_id, stdin, expected_outputs } = req.body;
+
+    const userId = req.user.id;
+
+    // Validate test cases
+    if (
+      !Array.isArray(stdin) ||
+      stdin.length === 0 ||
+      !Array.isArray(expected_outputs) ||
+      expected_outputs.length !== stdin.length
+    ) {
+      return res.status(400).json({
+        error: "Invalid or Missing test cases",
+        success: false,
+      });
+    }
+
+    if (!source_code || !language_id) {
+      return res.status(400).json({
+        error: "Invalid or Missing fields",
+        success: false,
+      });
+    }
+
+    // 2. Prepare each test cases for judge0 batch submission
+    const submissions = stdin.map((input) => ({
+      source_code,
+      language_id,
+      stdin: input,
+    }));
+
+    // 3. Send batch of submissions to judge0
+    const submitResponse = await submitBatch(submissions);
+
+    const tokens = submitResponse.map((res) => res.token);
+
+    // 4. Poll judge0 for results of all submitted test cases
+    const results = await pollBatchResults(tokens);
+
+    let allPassed = true;
+
+    const detailedResults = results.map((result, i) => {
+      const stdout = result.stdout?.trim() || null;
+      const expected_output = expected_outputs[i]?.trim();
+      const passed = stdout === expected_output;
+
+      if (!passed) allPassed = false;
+
+      return {
+        testCase: i + 1,
+        passed,
+        stdout,
+        expected: expected_output,
+        stderr: result.stderr || null,
+        compile_output: result.compile_output || null,
+        status: result.status.description,
+        memory: result.memory ? `${result.memory} KB` : undefined,
+        time: result.time ? `${result.time} s` : undefined,
+      };
+    });
+
+    let submission = {
+      userId,
+      sourceCode: source_code,
+      language: getLanguageName(language_id),
+      stdin: stdin.join("\n"),
+      stdout: JSON.stringify(detailedResults.map((res) => res.stdout)),
+      stderr: detailedResults.some((res) => res.stderr)
+        ? JSON.stringify(detailedResults.map((res) => res.stderr))
+        : null,
+      compileOutput: detailedResults.some((res) => res.compile_output)
+        ? JSON.stringify(detailedResults.map((res) => res.compile_output))
+        : null,
+      status: allPassed ? "Accepted" : "Wrong Answer", // Accepted, wrong
+      memory: detailedResults.some((res) => res.memory)
+        ? JSON.stringify(detailedResults.map((res) => res.memory))
+        : null,
+      time: detailedResults.some((res) => res.time)
+        ? JSON.stringify(detailedResults.map((res) => res.time))
+        : null,
+    };
+
+    const testCaseResults = detailedResults.map((result) => ({
+      submissionId: submission.id,
+      testCase: result.testCase,
+      passed: result.passed,
+      stdout: result.stdout,
+      expected: result.expected,
+      stderr: result.stderr,
+      compileOutput: result.compile_output,
+      status: result.status,
+      memory: result.memory,
+      time: result.time,
+    }));
+
+    submission.testcases = testCaseResults;
+
+    return res.status(200).json({
+      success: true,
+      message: "Code executed successfully",
+      submission,
+    });
+  } catch (error) {
+    console.error("Error running code:", error);
+    res.status(500).json({
+      error: "Failed to run code",
+      success: false,
+    });
+  }
+};
--- backend/src/routes/execute-code.routes.js
+++ backend/src/routes/execute-code.routes.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { executeCode, runCode } from "../controllers/executeCode.controller.js";
+import {
+  executeCode,
+  runCode,
+  runCodeForCollaborativeEditor,
+} from "../controllers/executeCode.controller.js";
 
 const executionRoute = express.Router();
 
 executionRoute.post("/", authMiddleware, executeCode);
 
 executionRoute.post("/run", authMiddleware, runCode);
+
+executionRoute.post(
+  "/collab-code",
+  authMiddleware,
+  runCodeForCollaborativeEditor
+);
 
 export default executionRoute;
