8cbe2f8477846063ffa3c34d8b52a93a0c01f64a
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-05-17 23:00:00 +0530

    feat: add rate limiting and IP request logging middleware

--- backend/index.js
+++ backend/index.js
@@ -11,19 +11,48 @@ import aiRoutes from "./src/routes/ai.routes.js";
 import likeRoutes from "./src/routes/like.routes.js";
 import savedProblemRoutes from "./src/routes/saveProblem.routes.js";
 import markedProblemForRevisionRoutes from "./src/routes/markedProblemForRevision.routes.js";
+import { rateLimit } from "express-rate-limit";
+import requestIp from "request-ip";
 
 dotenv.config();
 
 const app = express();
 
+app.use(requestIp.mw());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 200, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req) => {
+    console.log("req.clientIp", req.clientIp);
+    return req.clientIp;
+  },
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use(limiter);
+
 app.use(
   cors({
     origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
     credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
@@ -35,6 +64,13 @@ app.use("/api/v1/ai", aiRoutes);
 app.use("/api/v1/like", likeRoutes);
 app.use("/api/v1/save-problem", savedProblemRoutes);
 app.use("/api/v1/mark-problem", markedProblemForRevisionRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found !!",
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
